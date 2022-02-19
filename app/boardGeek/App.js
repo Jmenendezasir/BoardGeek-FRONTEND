bf928b7a0874943fd81540a1f706914df0b0a297
jmenendezasir <jmenendezasir@gmail.com>
2022-02-19 17:08:47 +0100

    Ejemplo de estados de objetos 18-02#5

--- app/boardGeek/App.js
+++ app/boardGeek/App.js
@@ -7,16 +7,65 @@
  */
 
 import React, {Component} from 'react';
-import {View } from 'react-native';
-import Header from './componentes/inicio'
+import {View, StyleSheet, Text, Button } from 'react-native';
+import Header from './componentes/inicio/header/header'
+import Main from './componentes/inicio/header/main'
 
 
-function App() {
-    return(
-    <View>
-      <Header />
-    </View>
-    );
+class App extends Component{
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0,
+    };
+
+    this.handleUp = this.handleUp.bind(this);
+    this.handleDown = this.handleDown.bind(this);
   }
 
+  handleUp() {
+    const { counter: ct } = this.state;
+    this.setState({ counter: ct + 1 });
+  }
+
+  handleDown() {
+    const { counter: ct } = this.state;
+    this.setState({ counter: ct - 1 });
+  }
+  render() {
+    const { counter } = this.state;
+  
+    return(
+      <View style={styles.container}>
+        <Header />
+        <Main />
+        <Button
+          title="+"
+          onPress={this.handleUp}
+        >
+        </Button>
+        <Button
+          title="-"
+          onPress={this.handleDown}
+        >
+        </Button>
+        <Text style={styles.contador}>{counter}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#273c75',
+  },
+  contador: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 25,
+    marginTop: 10,
+  },
+})
 export default App;
