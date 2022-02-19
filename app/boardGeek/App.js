d3ae7a70564fdc183281ab8b114445575c44f33e
jmenendezasir <jmenendezasir@gmail.com>
2022-02-19 15:09:16 +0100

    Demo funcionamiendo estilos CSS con y sin constantes 18-02#3

--- app/boardGeek/App.js
+++ app/boardGeek/App.js
@@ -6,107 +6,32 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import React, {Component} from 'react';
+import {View, StyleSheet} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+/** Podemos indicar los estilos desde el propio tag o desde las constantes que declaremos */
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+class App extends Component {
+  render() {
+    return(
+    <View style={styles.container}>
+      <View style={styles.containerOrange} />
+      <View style={styles.containerOrange} />
+      <View style={{ flex: 1, backgroundColor: 'green' }} />
     </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Hola mundo desde React Native">
-            Desarrollado por <Text style={styles.highlight}>Juan Carlos Mena</Text> y
-            <Text style={styles.highlight}>José Carlos Menéndez</Text>
-          </Section>
-          <Section title="Empezamos a desarrollar">
-            Si pulsas 2 veces la <Text style={styles.highlight}>R</Text> se recarga la página.
-          </Section>
-          <Section title="Y aprenderemos React Native">
-            En cambio, si pulsas <Text style={styles.highlight}>Ctrl + M</Text> o agitas el móvil se recarga la página.
-          </Section>
-          <Section title="Sin llorar mucho">
-            Porque es muy difícil :(
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
+    );
+  }
+}
+/** Podemos meter los estilos mediante constantes, para poder llamarlos así desde cada tag.*/
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1, 
+    backgroundColor: 'blue',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  containerOrange: {
+    flex: 1,
+    backgroundColor: 'orange',
+  }
 });
 
 export default App;
